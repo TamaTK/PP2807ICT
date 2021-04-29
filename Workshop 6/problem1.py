b7cfb1be6bf52f3cbea92e3cb55a7584d7b7a2be
Tamahou Scott <T2ks.wo@gmail.com>
2021-04-29 16:50:12 +1000

    Problem 1 of Workshop 6. Beginning our jump into complex data structures!

---
+++ Workshop 6/problem1.py
@@ -0,0 +1,4 @@
+# Problem: Write a program that prompts for the names of a source file to read and a target
+# file to write, and copy the content of the source file to the target file, but get all empty lines
+# removed, then output the number of empty lines removed. For example:
+#
