8fd8e07b6722e3a8f55fd85943beecd9c2a1db4a
Tamahou Scott <T2ks.wo@gmail.com>
2021-04-30 10:05:33 +1000

    Problem 1 of Workshop 6. Beginning our jump into complex data structures!

--- Workshop 6/problem1.py
+++ Workshop 6/problem1.py
@@ -1,4 +1,17 @@
 # Problem: Write a program that prompts for the names of a source file to read and a target
 # file to write, and copy the content of the source file to the target file, but get all empty lines
-# removed, then output the number of empty lines removed. For example:
-#
+# removed, then output the number of empty lines removed.
+
+# prompt
+srcPath = input("Source file name: ")
+trgPath = input("Target file name: ")
+
+# open
+try:
+    src = open(srcPath, 'r')
+    for line in src:
+        print(line, end="")
+    trg = open(trgPath, 'w')
+
+except:
+    print("Can't read/open file: ")
