c7061d4cdf3e1037c37028f329137644e3548193
Tamahou Scott <T2ks.wo@gmail.com>
2021-05-12 11:22:55 +1000

    Commenting

--- Workshop 7/problem1.py
+++ Workshop 7/problem1.py
@@ -14,3 +14,12 @@ def read_write(x, y):
 
 
 read_write(input("Source file name: "), input("Target file name: "))
+
+
+""" With statement supports any object with __enter__() and __exit__() methods. 
+When Python exists the "with" block, it will close the file after no matter how the block exits. Therefore,
+there is no need to close the file with .close()
+
+Function opens both paths and iterates through each line of the source file read. It then checks to see if
+there are any lines with whitespaces only and counts them. If this is not the case, function will write the specific
+line into the target file. Important to note that whatever is in the target file will be written over."""
