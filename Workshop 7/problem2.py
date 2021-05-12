e3a65c8afb53e69b197876e96ede1a5f7e953996
Tamahou Scott <T2ks.wo@gmail.com>
2021-05-12 11:52:00 +1000

    Solved problem2 with complete commenting.

--- Workshop 7/problem2.py
+++ Workshop 7/problem2.py
@@ -1,15 +1,18 @@
 # Problem: Write a program that prompts for the name of a file, then print the first two lines
 # and the last two lines of the file.
 
-# prompt
-srcFile = input("File name: ")
+def line_reader(x):
+    with open(x, 'r') as path:
+        lines = path.read().splitlines()
+        print(lines[0])
+        print(lines[1])
+        print(lines[-2])
+        print(lines[-1])
 
-# read
-src = open(srcFile, 'r')
-lines = src.readlines()
 
-lastLines = lines[1], lines[2]
-firstLines = lines[-2:]
+line_reader(input("File Name: "))
 
-print(lastLines)
-print(firstLines)
+
+""" Function uses user input as argument. Path is opened using 'with' statement. We create a list by method chaining 
+.read().splitlines() (reads contents of file and splits string into a list where each line is a list item).
+Finally, first two lines are printed, last two lines are printed. """
