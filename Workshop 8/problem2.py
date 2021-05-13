e4d964e97f3ce7fb4f59c2b42570df3ab3e3481c
Tamahou Scott <T2ks.wo@gmail.com>
2021-05-13 17:37:48 +1000

    Working on problem 2, workshop 8. Made things terribly complicated for myself. At least I know where I've gone wrong. XD

--- Workshop 8/problem2.py
+++ Workshop 8/problem2.py
@@ -1,16 +1,41 @@
-def list_compare(list1, list2):
-    new_list = []
-    for i in range(0, len(list1)):
-        for j in range(0, len(list2)):
-            if list1[i] == list2[j]:
-                new_list.append(list1[i])
+def numeric_check(lst):
+    for element in lst:
+        if element.isnumeric():
+            return True
+        else:
+            return False
 
-            else:
-                pass
-    return new_list
+
+def list_generate(string):
+    lst = string.split()
+    return lst
 
 
 def unique(lst):
     list_set = set(lst)
     unique_list = (list(list_set))
     return unique_list
+
+
+def list_compare(list1, list2):
+    new_list = []
+    for i in range(0, len(list1)):
+        for j in range(0, len(list2)):
+            if list1[i] == list2[j]:
+                new_list.extend(list1[i])
+            else:
+                pass
+    return new_list
+
+
+sentinel = 0
+
+while sentinel == 0:
+    first = list_generate(input("List 1: "))
+    if first:
+        second = list_generate(input("List 2: "))
+        mirrored_list = unique(
+            list_compare(first, second))
+        print(mirrored_list)
+    else:
+        sentinel = 1
